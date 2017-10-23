ef467c3b83cbf437c2e54b71c62d558922ac34a4
Mahmoud <15x0069@eng.asu.edu.eg>
2017-10-24 01:52:02 +0200

    Update SeProject.cpp

--- SeProject/src/SeProject.cpp
+++ SeProject/src/SeProject.cpp
@@ -11,13 +11,26 @@
 int main()
 {
 
-	string s1, s2, op;
+	string s1, s2, op,vName1,vName2;
 	float** A, ** B, ** C;
 	matrix M;
+	
+	cout <<"--------------------------Program Hints-------------------------------\n";
+	cout <<"----------------------------------------------------------------------\n";
+		cout <<"At Any time if you want to terminate the program type 'Exit' or 'Terminate' as an input \n";
+	cout <<"-----------------------------------------------------------------------\n";
+	cout <<"-----------------------------------------------------------------------\n";
+while(1)
+{
 
 	cout << "Please Enter The first Matrix In The Right Format: \t" << "like A=[1 2; 3 4]" << endl;    //insert A
-
+	
 	getline(cin, s1);
+	if(s1=="Exit" ||s1=="exit"||s1=="Terminate"||s1=="terminate")
+	{
+		return 0;;
+	}
+	vName1 = s1.substr(0,s1.find("="));
 	s1 = s1.substr(s1.find('['));
 	int m1 = M.Getrows(s1);
 	int n1 = M.Getcolumns(s1);
@@ -26,6 +39,11 @@ int main()
 
 	cout << "Please Enter The second Matrix In The Right Format: \t" << "like B=[1 2; 3 4]" << endl;   //insert B
 	getline(cin, s2);
+	if(s2=="Exit" ||s2=="exit"||s2=="Terminate"||s2=="terminate")
+	{
+		return 0;;
+	}
+	vName2 = s2.substr(0,s2.find("="));
 	s2 = s2.substr(s2.find('['));
 	int m2 = M.Getrows(s2);
 	int n2 = M.Getcolumns(s2);
@@ -34,20 +52,16 @@ int main()
 
 	cout << "please enter the required operation:\nlike:C=A+B  or\nlike:C=A-B  or\nlike:C=A*B or\nlike:C=A' or\nlike:C=B'" << endl;
 	getline(cin, op);
-	int pos1 = op.find('+');
-	int pos2 = op.find('-');
-	int pos3 = op.find('*');
-   	int pos4A = op.find("A'");
- 	int pos4B = op.find("B'");
-
-
-	if(pos4A != -1)
+	if(op=="Exit" ||op=="exit"||op=="Terminate"||op=="terminate")
+		return 0;
+	op=op.substr(op.find("=")+1);
+	if(op == (vName1+"'"))
     {
         C=M.Transpose_matrix(A,m1,n1);
         M.print_matrix(C,n1,m1);
         cout << "\r\n";
     }
-    else if(pos4B != -1)
+    else if(op==(vName2+"'"))
     {
         C=M.Transpose_matrix(B,m2,n2);
         M.print_matrix(C,n2,m2);
@@ -55,7 +69,7 @@ int main()
     }
 
 
-	else if (m1 == m2 && m1 !=0 && n1 == n2 && pos1 != -1)                    // both matrices dimensions must be equal
+	else if (op.find("+")!=-1 && m1 == m2 && m1 !=0 && n1 == n2)                    // both matrices dimensions must be equal
 	{
 		cout << "The summation result=" << endl;
 		C = M.sum_matrix(A, B, m1, n1);
@@ -63,15 +77,22 @@ int main()
 		cout << "\r\n";
 	}
 
-	else if (m1 == m2 && m1 !=0 && n1 == n2 && pos2 != -1)               // both matrices dimensions must be equal
+	else if (m1 == m2 && m1 !=0 && n1 == n2 && op==(vName1+"-"+vName2))               // both matrices dimensions must be equal
 	{
 		cout << "The subtraction result=" << endl;
 		C = M.sub_matrix(A, B, m1, n1);
 		M.print_matrix(C, m1, n1);
 		cout << "\r\n";
 	}
+	else if (m1 == m2 && m1 !=0 && n1 == n2 && op==(vName2+"-"+vName1))               // both matrices dimensions must be equal
+		{
+			cout << "The subtraction result=" << endl;
+			C = M.sub_matrix(B, A, m1, n1);
+			M.print_matrix(C, m1, n1);
+			cout << "\r\n";
+		}
 
-	else if (n1 == m2 && m2 !=0 && pos3 != -1)                      // column of matrix A must be equal to row of matrix B
+	else if ( op.find("*") != -1)                      // column of matrix A must be equal to row of matrix B
 	{
 		cout << "The multiplication result=" << endl;
 		C = M.multiply_matrix(A, B, m1, n2);
@@ -85,7 +106,7 @@ int main()
 		cout << "Oh!! Error Good Bye" << endl;
 
 
-
+	}
 
 	return 0;
 }
