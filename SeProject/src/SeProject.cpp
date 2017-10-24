ac23cb0ccfbb9df9f664647bd2f1b04d6b9d4835
ahmedwaelkahrba <31901821+ahmedwaelkahrba@users.noreply.github.com>
2017-10-24 20:59:24 +0300

    Update SeProject.cpp

--- SeProject/src/SeProject.cpp
+++ SeProject/src/SeProject.cpp
@@ -7,6 +7,7 @@
 
 
 #include "matrix.h"
+#include <fstream> //waels
 bool IS_terminated(string s1)
 {
 
@@ -24,7 +25,7 @@ void Error_message()
 	}
 int main()
 {
-
+	ifstream input(rightformat.txt); //waels
 	string s1, s2, op,vName1,vName2;
 	float** A, ** B, ** C;
 	matrix M;
@@ -34,21 +35,24 @@ int main()
 		cout <<"At Any time if you want to terminate the program type 'Exit' or 'Terminate' as an input \n";
 	cout <<"-----------------------------------------------------------------------\n";
 	cout <<"-----------------------------------------------------------------------\n";
-while(1)
-{
+//while(1){ waelsdelete 
+	cout << "the input is at file called (right format) in the required format" ;//wael
+	while (getline(input,s1)&&getline(input,s2)&&getline(input,op)){  //waels
 
-	cout << "Please Enter The first Matrix In The Right Format: \t" << "like A=[1 2; 3 4]" << endl;    //insert A
+
+	//cout << "Please Enter The first Matrix In The Right Format: \t" << "like A=[1 2; 3 4]" << endl;    //insert A
 	
-	getline(cin, s1);
+	//getline(cin, s1); waelsdelete
 	//to quit from the program if 'exit' is entered
 	if(IS_terminated(s1))
 		return 0;
 	//
 
-	if( s1 == ""  || s1.find("[") == -1)
+	if( s1 == ""  || s1.find("[") == -1) // l goz2 da kol mara hy3ml return 0 ella lw dakhlt l input fl file b special order waels
 	{
 	Error_message();//prints out to the screen error message
-	getline(cin, s1);
+	//getline(cin, s1); waelsdelete
+	input>>s1; //waels edit
 	if (s1=="y"||s1=="yes"|| s1=="Y"||s1=="Yes")
 	{
 		continue ;
@@ -63,10 +67,11 @@ while(1)
 		int m1 = M.Getrows(s1);
 	int n1 = M.Getcolumns(s1);
 //making sure the the matrix is not empty 
-	if( m1<=0)
+	if( m1<=0)				// l goz2 da kol mara hy3ml return 0 ella lw dakhlt l input fl file b special order waels
 	{
 		Error_message();
-		getline(cin, s1);
+		//getline(cin, s1);  waelsdelete
+		input>>s1; //waels
 		if (s1=="y"||s1=="yes"|| s1=="Y"||s1=="Yes")
 		{
 			continue ;
@@ -82,18 +87,19 @@ while(1)
 	A = M.create_matrix(m1, n1);
 	A = M.fill_matrix(s1, A, m1, n1);
 
-	cout << "Please Enter The second Matrix In The Right Format: \t" << "like B=[1 2; 3 4]" << endl;   //insert B
-	getline(cin, s2);
+	//cout << "Please Enter The second Matrix In The Right Format: \t" << "like B=[1 2; 3 4]" << endl;   //insert B
+	//getline(cin, s2);    		 waelsdelete
 	//checking if the user entered exit to terminate the program.
 		if(IS_terminated(s2))
 		{
 			return 0;
 		}
 
-		if( s2 == ""  || s2.find("[") == -1)
+		if( s2 == ""  || s2.find("[") == -1) 	 // l goz2 da kol mara hy3ml return 0 ella lw dakhlt l input fl file b special order waels
 	{
 		Error_message();//prints out an error message
-		getline(cin, s2);
+		//getline(cin, s2); waelsdelete
+		input>>s2; //waels
 		if (s2=="y"||s2=="yes"|| s2=="Y"||s2=="Yes")
 		{
 			continue ;
@@ -109,10 +115,11 @@ while(1)
 	int m2 = M.Getrows(s2);
 	int n2 = M.Getcolumns(s2);
 		//making sure the the matrix is not empty 
-	if( m2<=0)
+	if( m2<=0)		// l goz2 da kol mara hy3ml return 0 ella lw dakhlt l input fl file b special order waels
 	{
 		Error_message();
-		getline(cin, s2);
+		//getline(cin, s2); waels delete
+		input>>s2 //waels
 		if (s2=="y"||s2=="yes"|| s2=="Y"||s2=="Yes")
 		{
 			continue ;
@@ -129,12 +136,15 @@ while(1)
 	B = M.create_matrix(m2, n2);
 	B = M.fill_matrix(s2, B, m2, n2);
 
-	cout << "please enter the required operation:\nlike:C=A+B  or\nlike:C=A-B  or\nlike:C=A*B or\nlike:C=A' or\nlike:C=B'" << endl;
-	getline(cin, op);
+	//cout << "please enter the required operation:\nlike:C=A+B  or\nlike:C=A-B  or\nlike:C=A*B or\nlike:C=A' or\nlike:C=B'" << endl; waelsdelete
+	//getline(cin, op); waels delete
 		while(op.find("+")==-1&&op.find("-")&&op.find("*")&&op.find("'")&&op!="exit")
 		{
+			
+			// l goz2 da lazmlo ani adkhl l input fl file b special order waels
 			cout<<"OOPS.,Sorry you have enterd not valid or wrong operation,please try again considering this format \nlike:C=A+B  or\nlike:C=A-B  or\nlike:C=A*B or\nlike:C=A' or\nlike:C=B'\n";
-			getline(cin, op);
+			//getline(cin, op);
+			input>>op; //waels edit
 		}
 	if(IS_terminated(op))
 		return 0;
