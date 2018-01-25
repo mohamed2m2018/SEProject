53e581fd8aa36d474c176d3c587b1ab522682120
Abdelrahman Mohamadeen <33037931+Abdo-A@users.noreply.github.com>
2018-01-25 20:51:45 +0200

    Update matrix.cpp

--- SeProject/src/matrix.cpp
+++ SeProject/src/matrix.cpp
@@ -240,6 +240,23 @@ float** matrix::Sin_M(float** A ,int rows ,int columns)
 }
 
 
+float** matrix::Cos_M(float** A ,int rows ,int columns)
+{
+   float** result = createEmptyMatrix(rows, columns);
+
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < columns; j++)
+			{
+				result[i][j] = cos(A[i][j]);
+
+			}
+		}
+
+		return result;
+
+}
+
 
 void matrix::print_matrix(float** A, int rows, int columns)
 	{
